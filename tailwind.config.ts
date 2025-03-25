e60e940edf9ea91eaa6e32216e898f419d4b0373
Pablo Rodriguez Albarran <pablorodriguezalbarran2000@gmail.com>
2025-03-25 16:39:37 +0100

    Update dependencies and add new company logos and icons; refactor CSS variables for primary colors in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,39 +36,16 @@ export default {
         },
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        primary: {
-          DEFAULT: "var(--primary)",
-          foreground: "#fff",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
+
         muted: {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
         },
       },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        sm: "calc(var(--radius) - 10px)",
       },
     },
   },
@@ -78,7 +55,17 @@ export default {
         light: {
           colors: {
             primary: {
-              DEFAULT: "var(--primary)",
+              50: "#f0f6ff",
+              100: "#dbe8fe",
+              200: "#bfd7fe",
+              300: "#93bbfd",
+              400: "#609afa",
+              500: "#3b82f6",
+              600: "#2570eb",
+              700: "#1d64d8",
+              800: "#1e55af",
+              900: "#1e478a",
+              DEFAULT: "#3b82f6",
               foreground: "#fff",
             },
             danger: {
@@ -87,9 +74,14 @@ export default {
             },
             default: {
               DEFAULT: "#ffffff",
-              foreground: "var(--primary)",
+              foreground: "#3b82f6",
             },
           },
+        },
+      },
+      layout: {
+        radius: {
+          small: "6px",
         },
       },
     }),
