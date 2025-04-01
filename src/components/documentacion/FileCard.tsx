8374d45821e045a5393b656273a2ecbf698bcef1
Pablo Rodriguez Albarran <pablorodriguezalbarran2000@gmail.com>
2025-04-01 13:37:37 +0200

    Refactor FileCard component to support optional file selection and improve rendering logic

--- src/components/documentacion/FileCard.tsx
+++ src/components/documentacion/FileCard.tsx
@@ -33,8 +33,8 @@ export function FileCard({
   file: DocumentacionFile;
   view: "grid" | "list";
   userData: User;
-  selectedFiles: DocumentacionFile[];
-  handleSelectFile: (file: DocumentacionFile) => void;
+  selectedFiles?: DocumentacionFile[];
+  handleSelectFile?: (file: DocumentacionFile) => void;
 }) {
   const getFileIcon = (file: DocumentacionFile) => {
     switch (file.extension) {
@@ -67,70 +67,123 @@ export function FileCard({
     }
   };
 
-  const isSelected = selectedFiles.some((f) => f.id === file.id);
+  const isSelected =
+    selectedFiles && selectedFiles.some((f) => f.id === file.id);
+
+  if (selectedFiles && handleSelectFile) {
+    return (
+      <Checkbox
+        aria-label={file.id}
+        classNames={{
+          base: cn(
+            "inline-flex w-full max-w-full bg-content1",
+            "hover:bg-content2 items-center justify-start p-2",
+            "cursor-pointer rounded-lg gap-2"
+          ),
+          label: "w-full",
+        }}
+        isSelected={isSelected}
+        value={file.id}
+        onValueChange={() => handleSelectFile(file)}
+      >
+        <Card className="relative w-full">
+          <CardContent className="p-4">
+            <div className="flex items-start justify-between">
+              <div className="flex items-center gap-4">
+                <div className="relative">
+                  <Image
+                    src={getFileIcon(file) as string}
+                    alt={file.type}
+                    width={512}
+                    height={512}
+                    className="max-w-20 w-full h-full"
+                  />
+                </div>
+                <div className="w-full">
+                  <Tooltip
+                    color="primary"
+                    size="lg"
+                    radius="full"
+                    content={file.name}
+                    isDisabled={view === "list"}
+                  >
+                    <h3
+                      className={`block text-ellipsis overflow-hidden whitespace-nowrap font-semibold text-lg ${
+                        view === "grid" ? "max-w-64 w-full" : ""
+                      }`}
+                    >
+                      {file.name}
+                    </h3>
+                  </Tooltip>
+                  {file.size && (
+                    <p className="text-sm text-muted-foreground">
+                      {formatFileSize(file.size)}
+                    </p>
+                  )}
+                  {file.upload_date && (
+                    <p className="text-xs text-muted-foreground">
+                      Fecha de subida: {formatDateTime(file.upload_date)}
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-col justify-between">
+                <FileCardDropdown userData={userData as User} file={file} />
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </Checkbox>
+    );
+  }
 
   return (
-    <Checkbox
-      aria-label={file.id}
-      classNames={{
-        base: cn(
-          "inline-flex w-full max-w-full bg-content1",
-          "hover:bg-content2 items-center justify-start p-2",
-          "cursor-pointer rounded-lg gap-2"
-        ),
-        label: "w-full",
-      }}
-      isSelected={isSelected}
-      value={file.id}
-      onValueChange={() => handleSelectFile(file)}
-    >
-      <Card className="relative w-full">
-        <CardContent className="p-4">
-          <div className="flex items-start justify-between">
-            <div className="flex items-center gap-4">
-              <div className="relative">
-                <Image
-                  src={getFileIcon(file) as string}
-                  alt={file.type}
-                  width={512}
-                  height={512}
-                  className="max-w-20 w-full h-full"
-                />
-              </div>
-              <div className="w-full">
-                <Tooltip
-                  color="primary"
-                  size="lg"
-                  radius="full"
-                  content={file.name}
-                  isDisabled={view === "list"}
-                >
-                  <h3
-                    className={`block text-ellipsis overflow-hidden whitespace-nowrap font-semibold text-lg ${
-                      view === "grid" ? "max-w-64 w-full" : ""
-                    }`}
-                  >
-                    {file.name}
-                  </h3>
-                </Tooltip>
-                {file.size && (
-                  <p className="text-sm text-muted-foreground">
-                    {formatFileSize(file.size)}
-                  </p>
-                )}
-                {file.upload_date && (
-                  <p className="text-xs text-muted-foreground">
-                    Fecha de subida: {formatDateTime(file.upload_date)}
-                  </p>
-                )}
-              </div>
+    <Card className="relative w-full">
+      <CardContent className="p-4">
+        <div className="flex items-start justify-between">
+          <div className="flex items-center gap-4">
+            <div className="relative">
+              <Image
+                src={getFileIcon(file) as string}
+                alt={file.type}
+                width={512}
+                height={512}
+                className="max-w-20 w-full h-full"
+              />
             </div>
-            <div className="flex flex-col justify-between">
-              <FileCardDropdown userData={userData as User} file={file} />
+            <div className="w-full">
+              <Tooltip
+                color="primary"
+                size="lg"
+                radius="full"
+                content={file.name}
+                isDisabled={view === "list"}
+              >
+                <h3
+                  className={`block text-ellipsis overflow-hidden whitespace-nowrap font-semibold text-lg ${
+                    view === "grid" ? "max-w-64 w-full" : ""
+                  }`}
+                >
+                  {file.name}
+                </h3>
+              </Tooltip>
+              {file.size && (
+                <p className="text-sm text-muted-foreground">
+                  {formatFileSize(file.size)}
+                </p>
+              )}
+              {file.upload_date && (
+                <p className="text-xs text-muted-foreground">
+                  Fecha de subida: {formatDateTime(file.upload_date)}
+                </p>
+              )}
             </div>
           </div>
-        </CardContent>
-      </Card>
-    </Checkbox>
+          <div className="flex flex-col justify-between">
+            <FileCardDropdown userData={userData as User} file={file} />
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
