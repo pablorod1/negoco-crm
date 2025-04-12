7fdd2f34f1f390800493d26b5e82aeef155582c8
Pablo Rodriguez Albarran <pablorodriguezalbarran2000@gmail.com>
2025-04-12 12:09:25 +0200

    Refactor Header component styling; update background color for improved visibility and consistency.

--- src/components/core/Header.tsx
+++ src/components/core/Header.tsx
@@ -3,7 +3,7 @@ import NotificationsMenu from "./NotificationsMenu";
 
 export default function Header() {
   return (
-    <header className="border-b sticky top-0 bg-white/80 z-50 backdrop-blur-lg">
+    <header className=" sticky top-0 bg-[#f9f9f9]/90 z-50 backdrop-blur-lg">
       <div className="flex items-center justify-between px-6 py-4">
         <SidebarTrigger />
         <div className="flex items-center space-x-4">
