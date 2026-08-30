4b54e97acb2c1e2b3e4754d94d90ef7d9a0fb107
Pablo Rodriguez <pablorodriguezalbarran2000@gmail.com>
2026-08-30 21:13:19 +0200

    ocultar alerta de documentos

--- src/comparativas/components/details/MainView.tsx
+++ src/comparativas/components/details/MainView.tsx
@@ -12,7 +12,7 @@ import {
 import { Button } from "@/core/components/ui/button";
 import { Switch } from "@/core/components/ui/switch";
 import { ServiceInfo } from "@/comparativas/components/editComparativa/ServiceInfo";
-import { formatComission, formatDateTime } from "@/core/utils/format";
+import { formatDateTime } from "@/core/utils/format";
 import AvatarComponent from "@/core/components/AvatarComponent";
 import { getStatusBadge } from "@/core/hooks/use-status-badge";
 import {
@@ -32,7 +32,6 @@ import CompletarEstudioModal from "@/comparativas/components/editComparativa/Com
 import AddTramiteDialog from "@/tramites/components/createTramite/AddTramiteDialog";
 import TooltipComponent from "@/core/components/TooltipComponent";
 import { Link } from "next-view-transitions";
-import { AbarcaDocumentsNotice } from "@/comparativas/components/details/AbarcaDocumentsNotice";
 import { FilesList } from "@/comparativas/components/editComparativa/FilesList";
 import UploadComparativaFilesModal from "@/comparativas/components/editComparativa/UploadComparativaFilesModal";
 import ComparativaComissionsSection from "@/comparativas/components/editComparativa/ComparativaComissionsSection";
@@ -941,7 +940,7 @@ export default function MainView({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <AbarcaDocumentsNotice documents={comparativa.abarca_documents} />
+          {/* <AbarcaDocumentsNotice documents={comparativa.abarca_documents} /> */}
           <FilesList
             files={(comparativa.files as ComparativaFile[]) || []}
             comparativa_id={comparativa.id}
